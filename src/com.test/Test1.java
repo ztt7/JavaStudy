99e64ae77f712c7b27bb32b64b6de883139c0100
ztttaishiji <ztttaishiji@163.com>
2023-07-20 22:56:51 +0800

    20230720

---
+++ src/com.test/Test1.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import java.sql.*;
+import java.util.logging.Logger;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Test1.class.getName());
+        logger.info("我是一个日志");
+    }
+}
